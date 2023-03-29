77ee4355de0660e79c5be0c54a041632751461b8
heavyautumn <misaelvrivera@gmail.com>
2023-03-29 12:27:09 -0600

    init

---
+++ 01-spring-boot-overview/05-command-line-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,35 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    //inject properties for: coach.name  and team.name
+    @Value("${coach.name}")
+    private  String coachName;
+    @Value("${team.name}")
+    private  String teamName;
+    //exporse new enpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team name: "+ teamName;
+    }
+    //expose "/" that return "Hello world"
+    @GetMapping("/")
+    public  String sayHello(){
+        return "Hello world";
+    }
+    // expose a new endpoint for workout
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run hard 5k!";
+    }
+
+    // expose a new endpoint for fortune
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is yoour lucky day :)";
+    }
+}
